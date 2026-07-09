23aef33ee615b443f5919f8633ced718f2962c30
duane-edgington <duane@mbari.org>
2026-07-09 12:22:53 -0700

    update

--- stage_data.sh
+++ stage_data.sh
@@ -2,30 +2,33 @@
 # scipt to set up directories
 #
 
-mkdir $PWD/datasets/
-mkdir $PWD/datasets/savedir/
+## get test images for smoke test
 
-cp /mnt/PAM_Analysis/duane_scratch/images.tar.gz $PWD/datasets/savedir/
-cp /mnt/PAM_Analysis/duane_scratch/labels.tar.gz $PWD/datasets/savedir/
-cp /mnt/PAM_Analysis/duane_scratch/data.yaml $PWD/datasets/savedir/
+wget -q https://media.roboflow.com/notebooks/examples/dog-2.jpeg
+wget -q https://media.roboflow.com/notebooks/examples/dog-3.jpeg
+
+## get datasets
+
+mkdir -p $PWD/datasets/
+mkdir -p $PWD/datasets/savedir/
+
+cp /mnt/PAM_Analysis/duane_scratch/rfdetr/images.tar.gz $PWD/datasets/savedir/
+cp /mnt/PAM_Analysis/duane_scratch/rfdetr/labels.tar.gz $PWD/datasets/savedir/
+cp /mnt/PAM_Analysis/duane_scratch/rfdetr/data.yaml $PWD/datasets/savedir/
 
 tar xf $PWD/datasets/savedir/images.tar.gz --directory $PWD/datasets/savedir/
 tar xf $PWD/datasets/savedir/labels.tar.gz --directory $PWD/datasets/savedir/
 
 ## make the directories that yolov11 expects
-mkdir $PWD/datasets/train/
-mkdir $PWD/datasets/train/images/
-mkdir $PWD/datasets/train/labels/
-mkdir $PWD/datasets/test/
-mkdir $PWD/datasets/test/images/
-mkdir $PWD/datasets/test/labels/
-mkdir $PWD/datasets/val/
-mkdir $PWD/datasets/val/images/
-mkdir $PWD/datasets/val/labels/
-
-
-
-
+mkdir -p $PWD/datasets/train/
+mkdir -p $PWD/datasets/train/images/
+mkdir -p $PWD/datasets/train/labels/
+mkdir -p $PWD/datasets/test/
+mkdir -p $PWD/datasets/test/images/
+mkdir -p $PWD/datasets/test/labels/
+mkdir -p $PWD/datasets/val/
+mkdir -p $PWD/datasets/val/images/
+mkdir -p $PWD/datasets/val/labels/
 
 #move the data to the expected directories
 rsync -a "$PWD/datasets/savedir/images/train/" "$PWD/datasets/train/"
@@ -37,7 +40,7 @@ rsync -a "$PWD/datasets/savedir/images/test/" "$PWD/datasets/test/"
 rsync -a "$PWD/datasets/savedir/images/val/" "$PWD/datasets/valid/"
 #cp -r "$PWD/datasets/savedir/labels/val/" "$PWD/datasets/valid/labels"
 
-ls $PMD/datasets/
+ls "$PWD/datasets"
 
 
 
